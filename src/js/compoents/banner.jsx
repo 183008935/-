6dceba4b8a9e61575223c70cf077f33d15e5c5ab
mac <mac.@MacdeMacBook-Pro.local>
2020-02-13 12:26:45 +0800

    测试

--- src/js/compoents/banner.jsx
+++ src/js/compoents/banner.jsx
@@ -1,20 +1,26 @@
-require('../../css/banner.css');
-import Img1 from '../../img/2.png';
+require("../../css/banner.css");
+import Img1 from "../../img/2.png";
 
-        function Banner(){
-            return (
-           <div className="banner">
-           <div className="banner-me">
-           <div className="banner-photo">
-           <img src={Img1} alt="头像"/>
-           </div>
-           <div className="banner-desc">
-                 <h1>钟声-田家豪</h1>
-                 <p>一个努力努力很努力的年轻人</p>           
-           </div> 
-           </div>
-           </div>
-        )
-        }
-        
- export default  Banner;
+function Banner() {
+  return (
+    <div className="banner">
+      <div className="banner-me">
+        <div className="banner-photo">
+          <img src={Img1} alt="头像" />
+        </div>
+        <div className="banner-desc">
+          <h1>钟声-田家豪</h1>
+          <p>一个努力努力很努力的年轻人</p>
+          <p>
+            语雀-
+            <a target="_blank" href="https://www.yuque.com/dashboard/docs">
+              https://www.yuque.com/dashboard/docs
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
